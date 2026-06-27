e4bc48ff62369dbe0fc628700cb530a3441614c1
Mikhail Rapada <mprapada@student.hau.edu.ph>
2026-06-27 21:47:22 +0800

    Finished Portfolio Skeleton (No CSS)

--- Portfolio/Portfolio/urls.py
+++ Portfolio/Portfolio/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import home, projects, tutoring
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('projects/', projects, name='projects'),
+    path('tutoring/', tutoring, name='tutoring')
 ]
--- Portfolio/Portfolio/views.py
+++ Portfolio/Portfolio/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def tutoring(request):
+    return render(request, 'tutoring.html')
