5863e5905fb96f38ad6dadd1408070e4ccfd1a15
Mikhail Rapada <mprapada@student.hau.edu.ph>
2026-07-08 15:37:35 +0800

    Add About Me page with database-driven personal info
    - Added about_me (abt.me) view and URL to display PersonalInfo from the database
    - Created about_me.html template rendering name, summary, contact
      number, email, and address using template tags
    - Linked home.html's placeholder abt.me href to the new about_me page
    - Added CSS styling for the About Me page layout
    - Rewrote personal summary to be more detailed
    - Simplify project_detail view to use QuerySet .get(id=pk)
    - Adjust spacing on project_detail.html navigation link

--- Portfolio/showcase/models.py
+++ Portfolio/showcase/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Project(models.Model):
     project_name = models.CharField(max_length=200)
     description = models.TextField()
-    tech_stack = models.CharField(blank=True, null=True, max_length=300, help_text="e.g. Python, Django, SQLite")
+    tech_stack = models.CharField(blank=True, null=True, max_length=300)
     link = models.URLField(blank=True, null=True)
 
     def __str__(self):
@@ -19,7 +19,7 @@ class PersonalInfo(models.Model):
     summary = models.TextField()
     contact_number = models.CharField(max_length=20)
     email = models.EmailField()
-    address = models.CharField(max_length=255)
+    address = models.CharField(max_length=200)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
--- Portfolio/showcase/views.py
+++ Portfolio/showcase/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import Project, PersonalInfo
 
 def home(request):
@@ -8,13 +8,22 @@ def tutoring(request):
     return render(request, 'tutoring.html')
 
 def project_list(request):
-    projects = Project.objects.all()
-    return render(request, 'projects.html', {'projects': projects})
+    my_projects = Project.objects.all()
+    context = {
+        'projects': my_projects,
+    }
+    return render(request, 'projects.html', context)
 
 def project_detail(request, pk):
-    project = get_object_or_404(Project, pk=pk)
-    return render(request, 'project_detail.html', {'project': project})
+    project = Project.objects.get(id=pk)
+    context = {
+        'project': project,
+    }
+    return render(request, 'project_detail.html', context)
 
 def about_me(request):
     info = PersonalInfo.objects.first()
-    return render(request, 'about_me.html', {'info': info})
+    context = {
+        'info': info,
+    }
+    return render(request, 'about_me.html', context)
