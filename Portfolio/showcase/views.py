f9bf5fcd7ef0b6c41a451a858e664731522738bf
Mikhail Rapada <mprapada@student.hau.edu.ph>
2026-08-05 12:04:42 +0800

    Add Add_Project feature with Django Form and Function-Based Create View
    - Create ProjectForm in forms.py matching Project model fields
    - Add add_project view handling GET (blank form) and POST (validate + save)
    - Add /projects/add/ URL route
    - Create add_project.html template with styled form matching project cards
    - Add link to Add Project page from projects.html

--- Portfolio/showcase/views.py
+++ Portfolio/showcase/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Project, PersonalInfo
+from .forms import ProjectForm
+from .models import Project
+
 
 def home(request):
     return render(request, 'home.html')
@@ -27,3 +30,22 @@ def about_me(request):
         'info': info,
     }
     return render(request, 'about_me.html', context)
+
+def add_project(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            new_project = Project(
+                project_name=form.cleaned_data['project_name'],
+                description=form.cleaned_data['description'],
+                tech_stack=form.cleaned_data['tech_stack'],
+                link=form.cleaned_data['link'],
+            )
+            new_project.save()
+            return redirect('projects')
+    else:
+        form = ProjectForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'add_project.html', context)
